ef174054632d79f6e9b56bbfed4872d9e2a230da
Rajath-Acharya <rajath365@gmail.com>
2023-03-18 20:54:35 +0530

    adding bottom tabs navigation

--- src/navigation/GlobalNavigation.tsx
+++ src/navigation/GlobalNavigation.tsx
@@ -1,17 +1,43 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-import HomeScreen from '../screens/HomeScreen';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import FeedsScreen from '../screens/FeedsScreen';
 import LoginScreen from '../screens/LoginScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 
 const Stack = createNativeStackNavigator();
-
+const Tab = createBottomTabNavigator();
 const GlobalNavigation = () => {
   return (
     <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen
+        name="Home"
+        component={TabScreens}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{headerShown: false}}
+      />
     </Stack.Navigator>
+  );
+};
+
+const TabScreens = () => {
+  return (
+    <Tab.Navigator initialRouteName="Feed">
+      <Tab.Screen
+        name="Feed"
+        component={FeedsScreen}
+        options={{headerShown: false}}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{headerShown: false}}
+      />
+    </Tab.Navigator>
   );
 };
 
---
+++ src/screens/FeedsScreen.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const FeedsScreen = () => {
+  return <Text>FeedsScreen</Text>;
+};
+
+export default FeedsScreen;
