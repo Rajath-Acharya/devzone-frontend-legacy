3e8b911b44a01900a3c8a42fff665b4a875c17a8
Rajath-Acharya <rajath365@gmail.com>
2023-03-18 20:13:14 +0530

    adding screens + navigation

---
+++ src/navigation/GlobalNavigation.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import HomeScreen from '../screens/HomeScreen';
+import LoginScreen from '../screens/LoginScreen';
+
+const Stack = createNativeStackNavigator();
+
+const GlobalNavigation = () => {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default GlobalNavigation;
